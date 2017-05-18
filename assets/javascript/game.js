64ee1341b4c0086346f315bfcbc1362aef902f3e
Daniel Starks <daniel.starks@Daniels-MacBook-Pro.local>
2017-05-18 17:32:25 -0400

    fix function to reset letter to guess after win/loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,63 @@
-var winCounter = 0;
-var lossCounter = 0;
-var livesCounter = 10;
+
 
 window.onload = function computerLetter() {
-
+	//set alphabet for letter choices
     var computerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    var computerGuess = computerLetters[Math.floor(Math.random() * computerLetters.length)];
-    document.getElementById("lives").innerHTML = livesCounter;
-    console.log(computerGuess);
+    
+    //setting counters up for start
+    var winCounter = 0;
+	var lossCounter = 0;
+	var livesCounter = 10;
+	var computerGuess = "";
 
-	
+	//computer picks a letter
+    function newLetter() {
+    	 computerGuess = computerLetters[Math.floor(Math.random() * computerLetters.length)];
+         console.log(computerGuess);
+    }
+    newLetter();
+
+
+    //Place livesCounter into the lives Div
+    document.getElementById("lives").innerHTML = livesCounter;
 
 	document.onkeyup = function(event) {
-
+		//User guesses and forces lowercase
 		var pressedKey = event.key;
 		var userGuess = pressedKey.toLowerCase();
+
+		//Setting up counters and guessed letters
 		var guessedLetters = document.getElementById("lettersGuessed");
 		document.getElementById("wins").innerHTML = winCounter;
 		document.getElementById("losses").innerHTML = lossCounter;
 		console.log(userGuess);
-
-		if (livesCounter > 0) {
-			if (userGuess === computerGuess) {
-				document.getElementById("wins").innerHTML = winCounter++;
-				document.getElementById("lives").innerHTML = livesCounter--;
-			}
-			else if (userGuess != computerGuess) {
-				document.getElementById("losses").innerHTML = lossCounter++;
-				document.getElementById("lives").innerHTML = livesCounter--;
-				guessedLetters.innerHTML = guessedLetters.innerHTML + userGuess + ", ";
-			}
-		}
-		else if (lossCounter == 10) {
-			winCounter = 0;
-			lossCounter = 0;
-			livesCounter = 10;
-		}
-
-
 		
 
+		if (livesCounter > 0) {
+            livesCounter--;
+            if (userGuess === computerGuess) {
+                winCounter++;
+                document.getElementById("wins").innerHTML = winCounter;
+                document.getElementById("lives").innerHTML = livesCounter;
+                newLetter();
+            }
+            else if (userGuess != computerGuess) {
+                lossCounter++;
+                document.getElementById("losses").innerHTML = lossCounter;
+                document.getElementById("lives").innerHTML = livesCounter;
+                guessedLetters.innerHTML = guessedLetters.innerHTML + userGuess + ", ";                
+            }
+        }
+        else {
+            winCounter = 0;
+            lossCounter = 0;
+            livesCounter = 10;
+            document.getElementById("wins").innerHTML = winCounter;
+            document.getElementById("lives").innerHTML = livesCounter;
+            document.getElementById("losses").innerHTML = lossCounter;
+            guessedLetters.innerHTML = "";
+            newLetter();
+
+        }
 	}
-
-
 }
